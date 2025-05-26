6291b04891ec07045342389a25f9ae1575845c4c
Alfriyan Shafteri <mangdaskim123@gmail.com>
2025-05-26 12:33:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -218,7 +218,7 @@
             src="${iframeURL}" 
             allow="encrypted-media; autoplay"
             width="100%" 
-            height="360" 
+            height="100%" 
             margin: 0 auto;
             frameborder="0" 
             allowfullscreen
